a72a0e76e24d1871566a20cb6d469ee42602e6d0
VINUHUB <aladoemeka1@gmail.com>
2025-11-10 17:28:02 -0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+  },
+};
